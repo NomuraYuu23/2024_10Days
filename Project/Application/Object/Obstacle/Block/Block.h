6a52dede2eb119de012d3551d9878c33b0c5db2c
野村優羽 <@MSI>
2024-09-04 13:57:35 +0900

    block型作成

---
+++ Project/Application/Object/Obstacle/Block/Block.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "../BaseObstacle.h"
+#include "../../../../Engine/Animation/Animation.h"
+class Block :
+    public BaseObstacle
+{
+
+public: // ベースのメンバ関数
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize(LevelData::MeshData* data) override;
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update() override;
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="viewProjection">ビュープロジェクション(参照渡し)</param>
+	void Draw(BaseCamera& camera) override;
+
+	/// <summary>
+	/// ImGui描画
+	/// </summary>
+	void ImGuiDraw() override;
+
+	/// <summary>
+	/// 衝突処理
+	/// </summary>
+	/// <param name="colliderPartner"></param>
+	/// <param name="collisionData"></param>
+	void OnCollision(ColliderParentObject colliderPartner, const CollisionData& collisionData) override;
+
+private: // パーツ,アニメーション変数
+
+	// 現在のモーション番号
+	uint32_t currentMotionNo_;
+
+	// 前のモーション番号
+	uint32_t prevMotionNo_;
+
+	//ノードアニメーション
+	Animation animation_;
+
+	// ローカル行列
+	std::unique_ptr<LocalMatrixManager> localMatrixManager_ = nullptr;
+
+
+};
+
