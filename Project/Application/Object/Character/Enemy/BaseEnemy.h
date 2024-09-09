8bf925a55c76d2ea08e8b3841ebebaab484f38bc
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2024-09-09 22:02:36 +0900

    外部で一括管理するようにEnemyに基底クラス追加

--- Project/Application/Object/Character/Enemy/BaseEnemy.h
+++ Project/Application/Object/Character/Enemy/BaseEnemy.h
@@ -3,19 +3,12 @@
 #include "../../../../Engine/Animation/Animation.h"
 #include "../../Obstacle/Block/BlockManager.h"
 #include "../Player/Player.h"
-#include "BaseEnemy.h"
 
-class FlyEnemy :
-	public BaseEnemy
+class BaseEnemy :
+	public MeshObject
 {
 
 public: // 静的メンバ関数
-
-	/// <summary>
-	/// 弾の生成
-	/// </summary>
-	/// <returns></returns>
-	static LevelData::MeshData FlyEnemyCreate();
 
 
 public: // ベースのメンバ関数
@@ -23,43 +16,43 @@ public: // ベースのメンバ関数
 	/// <summary>
 	/// 初期化
 	/// </summary>
-	void Initialize(LevelData::MeshData* data) override;
+	virtual void Initialize(LevelData::MeshData* data) override {};
 
 	/// <summary>
 	/// 更新
 	/// </summary>
-	void Update() override;
+	virtual void Update() override {};
 
 	/// <summary>
 	/// 描画
 	/// </summary>
 	/// <param name="viewProjection">ビュープロジェクション(参照渡し)</param>
-	void Draw(BaseCamera& camera);
+	virtual void Draw(BaseCamera& camera) {};
 
 	/// <summary>
 	/// ImGui描画
 	/// </summary>
-	void ImGuiDraw() override {};
+	virtual void ImGuiDraw() override {};
 
 	/// <summary>
 	/// 衝突処理
 	/// </summary>
 	/// <param name="colliderPartner"></param>
 	/// <param name="collisionData"></param>
-	void OnCollision(ColliderParentObject colliderPartner, const CollisionData& collisionData);
+	virtual void OnCollision(ColliderParentObject colliderPartner, const CollisionData& collisionData) {};
 
 	/// <summary>
 	/// パーティクル描画
 	/// </summary>
 	/// <param name="camera">カメラ</param>
-	void ParticleDraw(BaseCamera& camera) override;
+	virtual void ParticleDraw(BaseCamera& camera) override {};
 
 private: // パーツ構成関数
 
 	/// <summary>
 	/// コライダー更新
 	/// </summary>
-	void ColliderUpdate();
+	virtual void ColliderUpdate() {};
 
 
 
@@ -72,30 +65,21 @@ private: // 衝突処理
 	/// </summary>
 	/// <param name="colliderPartner"></param>
 	/// <param name="collisionData"></param>
-	void OnCollisionObstacle(ColliderParentObject colliderPartner, const CollisionData& collisionData);
+	virtual void OnCollisionObstacle(ColliderParentObject colliderPartner, const CollisionData& collisionData) {};
 
 private: // 関数
 
-	/// <summary>
-	/// 範囲外削除
-	/// </summary>
-	void PositionLimit();
-
 private: //	変数
 
-	// 速度
-	Vector3 velocity_ = {};
+protected:
 
-	// 速度
-	float speed_ = 0.3f;
+	//フラグ
+	//死亡アニメーションが再生されているかどうか、これが立った時点でenemyManagerのリストから外す
+	bool isPlayDeathAnimation_ = false;
+
 
 public: // アクセッサ
 
-	WorldTransform* GetWorldTransformAdress() { return &worldTransform_; }
-
-	Vector3 GetVelocity() { return velocity_; }
-
-	void SetVelocity(const Vector3& velocity) { velocity_ = velocity; }
 
 };
 
